9b24c707204e6e0f2aca62b0e6b2ad14555ab719
themesoftdev <themesoftdev@gmail.com>
2015-08-13 12:19:49 +0900

    Initial commit

---
+++ app/src/main/java/com/somad/fes/MainActivity.java
@@ -0,0 +1,24 @@
+package com.somad.fes;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onHall(View v){
+        Intent intent_hall  = new Intent(getApplicationContext(), Hall.class);
+        startActivity(intent_hall);
+    }
+}
